3ddbe690384d125562aafec9074576157f4c41b4
Psh <dev@mtbdx.com>
2017-04-19 22:27:02 +0300

    Correct displaying of 2030-12-31 date (empty) and XXXX-12-31 (only year); Fix 'both_types' method

--- models.rb
+++ models.rb
@@ -8,10 +8,28 @@ class Album < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   def both_types
-    if self.secondary_type && !self.secondary_type.empty?
-      return "#{primary_type} + #{secondary_type}"
+    if primary_type && !primary_type.empty?
+      if secondary_type && !secondary_type.empty?
+        return "#{primary_type} + #{secondary_type}"
+      else
+        return primary_type
+      end
     else
-      return primary_type
+      if secondary_type && !secondary_type.empty?
+        return secondary_type
+      else
+        return "NO TYPE"
+      end
+    end
+  end
+
+  def fix_date
+    if date.year >= 2030
+      return ''
+    elsif date.month == 12 && date.day == 31
+      return date.year.to_s
+    else
+      return date.to_s
     end
   end
 end
