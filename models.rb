eb7a0063217b5fafd2bce75e5db8b60945860d94
Psh <dev@mtbdx.com>
2018-01-31 21:43:25 +0300

    Add links to jump from artist to performer and back

--- models.rb
+++ models.rb
@@ -204,6 +204,25 @@ class Record < ActiveRecord::Base
     File.join('/rdownload', directory, filename)
   end
 
+  def update_mediainfo
+    return unless File.exist?(self.full_path)
+    m = MediaInfoNative::MediaInfo.new()
+    m.open_file(self.full_path)
+    self.mediainfo = {
+      dur: m.audio.duration,
+      br:  m.audio.bit_rate,
+      brm: m.audio.bit_rate_mode,
+      sr:  m.audio.sample_rate,
+      ch:  m.audio.channels
+    }
+    m.close
+  end
+
+  def update_mediainfo!
+    self.update_mediainfo
+    self.save
+  end
+
   def delete_from_filesystem
     File.delete(full_path) if File.exist?(full_path)
     self.delete
