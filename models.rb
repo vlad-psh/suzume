201827f6ab909fce4d732850e50c4974a442cbb1
Psh <dev@mtbdx.com>
2018-03-08 20:52:47 +0300

    Remove deprecated Artist/Album/Track models; Feat: tulip.yml ++
    tulip.yml details:
    * It saves content of current directory
    * For audio files, all necessary information is saved (bitrate, duration, etc)
    * When directory already contains tulip.yml, it reads content of
      this file so that there is no need to get mediainfo (bitrate, etc) again
    * folder.slim displays simple list of files found in tulip.yml

--- models.rb
+++ models.rb
@@ -1,204 +1,5 @@
-module MusicObject
-  def self.find(obj_type, obj_id)
-    return case obj_type.to_sym
-      when :p then Artist.find(obj_id)
-      when :r then Album.find(obj_id)
-      when :t then Track.find(obj_id)
-      else throw StandardError.new("Unknown parent_type: #{obj_type}")
-    end
-  end
-
-  def simple_type
-    if self.kind_of?(Artist)
-      return :p
-    elsif self.kind_of?(Album)
-      return :r
-    elsif self.kind_of?(Track)
-      return :t
-    else
-      throw StandardError.new("Unknown MusicObject type: #{self.class}")
-    end
-  end
-
-  def notes
-    return Note.where(parent_type: simple_type, parent_id: id)
-  end
-end
-
-class Artist < ActiveRecord::Base
-  include MusicObject
-  has_and_belongs_to_many :albums
-
-  def full_path
-    return @_full_path if @_full_path
-    @_full_path = File.join($library_path, filename)
-  end
-
-  def xdestroy!
-    if Performer.find_by(old_id: self.id)
-      self.albums.each do |a|
-        # destroy only 'not processed' albums
-        a.xdestroy! unless Release.find_by(old_id: a.id)
-      end
-      self.status = 'processed'
-      self.save
-    else
-      self.albums.each {|a| a.xdestroy!}
-      self.notes.destroy_all
-
-      self.destroy
-    end
-  end
-end
-
-class Album < ActiveRecord::Base
-  include MusicObject
-  has_and_belongs_to_many :artists
-  has_many :tracks
-
-  def full_path
-    return @_full_path if @_full_path
-    _artist = artists.first
-    @_full_path = File.join(_artist.full_path, filename)
-  end
-
-  def search_tracks
-    old_pwd = Dir.pwd
-    Dir.chdir(full_path)
-    tracks  = Dir.glob("**/*.mp3", File::FNM_CASEFOLD)
-    tracks += Dir.glob("**/*.m4a", File::FNM_CASEFOLD)
-    Dir.chdir(old_pwd)
-    return tracks.sort
-  end
-
-  def all_tracks
-    if Dir.exist?(full_path)
-      filenames = search_tracks
-
-      tracks.each do |t|
-        filenames.delete(t.filename) if filenames.include?(t.filename)
-      end
-
-      filenames.each do |fn|
-        tracks << Track.create(album: self, filename: fn)
-      end
-    end
-
-    return tracks.order(filename: :asc)
-  end
-
-  def cover_path
-    jpg_path = File.expand_path("orig/#{self.id}.jpg", $covers_path)
-    return jpg_path if File.exist?(jpg_path)
-
-    png_path = File.expand_path("orig/#{self.id}.png", $covers_path)
-    return png_path if File.exist?(png_path)
-
-    return nil
-  end
-
-  def thumb_path
-    jpg_path = File.expand_path("thumb/#{self.id}.jpg", $covers_path)
-    return jpg_path if File.exist?(jpg_path)
-    
-    png_path = File.expand_path("thumb/#{self.id}.png", $covers_path)
-    return png_path if File.exist?(png_path)
-
-    return nil
-  end
-
-  def xdestroy!
-    throw StandardError.new("Linked to Release") if Release.find_by(old_id: self.id)
-
-    self.tracks.each {|t| t.xdestroy!}
-    self.notes.destroy_all
-
-    if File.exists?(self.full_path)
-      removed_artist_path = File.join($library_path, 'removed', self.artists.first.filename)
-      FileUtils.mkdir(removed_artist_path) unless File.exists?(removed_artist_path)
-
-      FileUtils.mv(self.full_path, removed_artist_path) # move
-
-      removed_album_path = File.join($library_path, 'removed',
-                self.artists.first.filename,
-                "#{self.year} #{self.title} [#{self.primary_type}]".gsub(/\//, '_'))
-      FileUtils.mv(File.join(removed_artist_path, self.filename), removed_album_path) # rename to "year title [type]"
-    end
-
-    self.destroy
-  end
-
-  def update_mediainfo!
-    all_tracks.each do |t|
-      t.update_mediainfo!
-    end
-  end
-
-  def create_folder!
-    FileUtils.mkdir_p(self.full_path) unless File.exist?(self.full_path)
-  end
-end
-
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :performers
-end
-
-class Track < ActiveRecord::Base
-  include MusicObject
-  belongs_to :album
-  before_create :update_mediainfo
-
-  def lyrics
-    if self.lyrics_json
-      return JSON.parse(self.lyrics_json)
-    else
-      return {}
-    end
-  end
-
-  def lyrics=(val)
-    self.lyrics_json = val.to_json
-  end
-
-  def full_path
-    return @_full_path if @_full_path
-    @_full_path = File.join(album.full_path, filename)
-  end
-
-  def update_mediainfo
-    return unless File.exist?(self.full_path)
-    m = MediaInfoNative::MediaInfo.new()
-    m.open_file(self.full_path)
-    self.mediainfo = {
-      dur: m.audio.duration,
-      br:  m.audio.bit_rate,
-      brm: m.audio.bit_rate_mode,
-      sr:  m.audio.sample_rate,
-      ch:  m.audio.channels
-    }
-    m.close
-  end
-
-  def update_mediainfo!
-    self.update_mediainfo
-    self.save
-  end
-
-  def xdestroy!
-    self.notes.destroy_all
-    self.destroy
-  end
-end
-
-class Note < ActiveRecord::Base
-  def parent
-    result = case parent_type
-      when :p then Artist.find(parent_id)
-      when :r then Album.find(parent_id)
-      when :t then Track.find(parent_id)
-      else throw StandardError.new("Unknown parent_type: #{parent_type}")
-    end
-  end
 end
 
 class Performer < ActiveRecord::Base
@@ -307,10 +108,20 @@ class Folder < ActiveRecord::Base
   end
 
   def subfolders
+    update_content! unless @_subfolders # update content and set @_subfolders
+
+    return @_subfolders
+  end
+
+  def update_content!
     skip_folders = Folder.where(folder_id: self.id).pluck(:path).map {|p| File.basename(p)}
 
     Dir.children(self.full_path).each do |c|
-      next if skip_folders.include?(c)
+      if skip_folders.include?(c)
+        skip_folders.delete(c)
+        next
+      end
+
       next unless File.directory?( File.join(self.full_path, c) )
 
       f = Folder.create(
@@ -320,6 +131,70 @@ class Folder < ActiveRecord::Base
       )
     end
 
-    return Folder.where(folder_id: self.id).order(path: :asc)
+    # TODO: remove Folder objects, that are still in skip_folders array; RECURSIVELY!
+
+    @_subfolders = Folder.where(folder_id: self.id).order(path: :asc)
+  end
+
+  def tulip_yml_path
+    File.join(self.full_path, 'tulip.yml')
+  end
+
+  def tulip_yml
+    return @_tulip_yml || self.get_tulip_yml
+  end
+
+  def get_tulip_yml
+    @_tulip_yml = File.exist?(self.tulip_yml_path) ?
+                    YAML.load(File.open(self.tulip_yml_path)) :
+                    {notes: [], files: {}}
+    return @_tulip_yml
+  end
+
+  def save_tulip_yml!
+    return unless tulip_yml.present?
+
+    File.open(self.tulip_yml_path, 'w') do |f|
+      f.write(tulip_yml.to_yaml)
+    end
+  end
+
+  def get_files
+    files = tulip_yml[:files].map {|path,details| path}
+
+    Dir.children(self.full_path).each do |c|
+      c_fullpath = File.join(self.full_path, c)
+      next if File.directory?(c_fullpath)
+      next if c =~ /.*tulip\.yml$/
+
+      if files.include?(c)
+        files.delete(c)
+        next
+      end
+
+      if c =~ /\.(mp3|m4a)/i
+        m = MediaInfoNative::MediaInfo.new()
+        m.open_file(c_fullpath)
+        info = {
+          type: 'audio',
+          rating: nil,
+          dur: m.audio.duration,
+          br:  m.audio.bit_rate,
+          brm: m.audio.bit_rate_mode,
+          sr:  m.audio.sample_rate,
+          ch:  m.audio.channels
+        }
+      elsif c=~ /\.(png|jpg|jpeg)/i
+        info = {
+          type: 'image'
+        }
+      end
+      info ||= {}
+      info[:size] = File.size(c_fullpath)
+
+      tulip_yml[:files][c] = info
+    end
+
+    self.save_tulip_yml!
   end
 end
