b554e592ccbe6fb033b82b139e085f9cb9d77366
Psh <dev@mtbdx.com>
2023-01-06 00:33:21 +0900

    Automatically set axios API_URL_BROWSER based on current domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,11 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/auth.js', '@/plugins/player.js'],
+  plugins: [
+    '@/plugins/auth.js',
+    '@/plugins/player.js',
+    '@/plugins/axios.client.js',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
